20216b74f966236eba57eda821b7a96fbad58739
henrique-paes <henrique.paes@shopper.com.br>
2023-02-10 17:07:19 -0300

    feature: adicionar obtencao do codigo de autorizacao

--- blueprints/user/user.py
+++ blueprints/user/user.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+import os
+import requests
+from urllib.parse import urlencode
+from flask import Blueprint, redirect, request
 
 bp_user = Blueprint("Users", __name__, url_prefix="/")
 
@@ -12,3 +15,26 @@ def get_users():
         'name': name,
         'token': token
     }
+
+@bp_user.route("users/authorize", methods=['GET'])
+def authorize():
+
+    # check if request is a google callback
+    url_params = request.args
+    if url_params.get('code'):
+        return {
+            'authorization_code': url_params['code']
+        }
+
+    # redirect authorization to google
+    params = {
+        "client_id": os.getenv('GOOGLE_CLIENT_ID'),
+        "scope": 'profile https://www.googleapis.com/auth/gmail.send',
+        "include_granted_scopes": 'true',
+        "response_type": 'code',
+        "access_type": 'offline',
+        "redirect_uri": 'http://localhost:5000/users/authorize'
+    }
+
+    url = 'https://accounts.google.com/o/oauth2/v2/auth?' + urlencode(params)
+    return redirect(location=url)
