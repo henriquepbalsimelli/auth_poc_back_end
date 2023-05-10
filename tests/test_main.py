de1f703144383372bf828946db5e4d90346e0746
henriquePedrozo <henrique.pedrozo@shopper.com.br>
2023-05-10 16:37:44 -0300

    feat: cria teste para verificar porcentagem

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,50 @@
-import requests
-
-
 def test_sum() -> None:
     """ asd """
     assert 2+2 == 4
 
+ 
+def test_sum2() -> None:
+    """ asd """
+    assert 2+2 == 4
+
+ 
+def test_sum3() -> None:
+    """ asd """
+    assert 2+2 == 4
+
+ 
+def test_sum4() -> None:
+    """ asd """
+    assert 2+2 == 4
+
+ 
+def test_sum5() -> None:
+    """ asd """
+    assert 2+2 == 4
+
+ 
+def test_sum6() -> None:
+    """ asd """
+    assert 2+2 == 4
+
+ 
+def test_sum7() -> None:
+    """ asd """
+    assert 2+2 == 4
+
+ 
+def test_sum8() -> None:
+    """ asd """
+    assert 2+2 == 4
+
+ 
+def test_sum9() -> None:
+    """ asd """
+    assert 2+2 == 3
+
+ 
+def test_sum10() -> None:
+    """ asd """
+    assert 2+2 == 3
+
+ 
