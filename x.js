398122cfb1ef2c65371e2905609dbb84dd60ca74
henriquePedrozo <henrique.pedrozo@shopper.com.br>
2023-05-10 12:59:12 -0300

    altera script

--- x.js
+++ x.js
@@ -1,19 +1,32 @@
-let comment = '';
-if (coveragePercentage >= 80) {
-    comment = `:white_check_mark: Code coverage: ${coveragePercentage}% - All tests passed!`;
-} else {
-    comment = `:x: Code coverage: ${coveragePercentage}% - Please improve test coverage.`;
-}
+const functionn = async() => {
 
-await octokit.request('PATCH /repos/henriquepbalsimelli/auth_poc_back_end/pulls/{pull_number}', {
-    owner: 'henriquepbalsimelli',
-    repo: 'auth_poc_back_end',
-    pull_number: { prNumber },
-    title: { comment },
-    body: { comment },
-    state: 'open',
-    base: 'master',
-    headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
+    const { Octokit } = require("@octokit/rest");
+    const token = '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';
+    const octokit = new Octokit({ auth: token });
+    const prNumber = 1080;
+    const coveragePercentage = 60;
+    
+    let comment = '';
+    if (coveragePercentage >= 80) {
+        comment = `:white_check_mark: Code coverage: ${coveragePercentage}% - All tests passed!`;
+    } else {
+        comment = `:x: Code coverage: ${coveragePercentage}% - Please improve test coverage.`;
     }
-})
+    
+    const response = await octokit.request(`PATCH /repos/henriquepbalsimelli/auth_poc_back_end/pulls/${prNumber}`, {
+        owner: 'henriquepbalsimelli',
+        repo: 'auth_poc_back_end',
+        pull_number: { prNumber },
+        title: 'new title',
+        body: { comment },
+        state: 'open',
+        base: 'master',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+
+    console.log(response)
+} 
+
+functionn()
